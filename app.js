713cfee68268af903e05b7bd3bf612009e150404
windhamdavid <david@davidawindham.com>
2012-05-10 13:55:13 -0400

    express-

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
+var flatiron = require('flatiron'),
+     app = flatiron.app; 
+ 
+ app.use(flatiron.plugins.http);
+
+
 var pub = __dirname + '/public';
 var Slide = require('./slide');
 var Presentation = require('./presentation');
 var PresentationController = require('./PresentationController');
 
-var express = require('express')
-        , app = express.createServer();
+var flatiron = require('flatiron'),
+     app = flatiron.app; 
+ 	 app.use(flatiron.plugins.http);
 
 var presentationController = new PresentationController();
 
 var createPresentationNodejs = [];
-createPresentationNodejs.push(new Slide("intro","Creating a presentation with Node.js"));
-createPresentationNodejs.push(new Slide("goal","Goal"));
-createPresentationNodejs.push(new Slide("requirements","Requirements"));
-createPresentationNodejs.push(new Slide("technology","technology"));
-createPresentationNodejs.push(new Slide("urlhandling","Configure url handling"));
-createPresentationNodejs.push(new Slide("handlerequests","Handle requests"));
-createPresentationNodejs.push(new Slide("viewcontent","View content"));
-createPresentationNodejs.push(new Slide("thefuture","The Future"));
-createPresentationNodejs.push(new Slide("questions","Questions"));
+createPresentationNodejs.push(new Slide("intro","Creating a real time presentation with Node.js"));
+createPresentationNodejs.push(new Slide("two","two"));
 
 presentationController.addPresentation(new Presentation("Creating a presentation with Node.js","create-presentation-nodejs",createPresentationNodejs,
     "For my work I had to prepare a short presentation of around 10 minutes. I decided to create a tool to create presentation " +
@@ -38,5 +38,5 @@ app.get('/presentation/:urlIdentifier/slide/:id', presentationController.slide);
 app.post('/presentation/:urlIdentifier/slide/:id', presentationController.command);
 app.get('/presentation/:urlIdentifier/all', presentationController.allSlides);
 
-app.listen(8888);
+app.listen(8018);
 console.log('Express server started on port %s', app.address().port);
